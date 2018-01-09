0297cdf30923bbf63cc79efeef455b94c57bba45
madhanrko <31653126+madhanrko@users.noreply.github.com>
2018-01-09 16:59:33 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf(" give the number");
+scanf("%d",&n);
+if(n<0)
+{
+printf(" %d is  negative number");
+}
+else
+{
+printf(" %d is a positive number");
+}
+}
